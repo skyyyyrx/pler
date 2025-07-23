eefe1daaf13c0f1f2268e61e58121cb58bbaf9ae
skyyyyrx <guanavera@outlook.com>
2025-07-23 11:20:22 +0700

    Update daytoni.py

--- daytoni.py
+++ daytoni.py
@@ -7,12 +7,13 @@ config = DaytonaConfig(api_key="dtn_65c10bd3da5ba392171806def2b21f0ad1013852708f
 daytona = Daytona(config)
 
 # Create the Sandbox instance
-sandbox = daytona.create()
+sandbox = daytona.create(
 resources=Resources(
-        cpu=4,         # 4 vCPUs
-        memory=8,      # 8 GiB RAM
+        cpu=10,         # 4 vCPUs
+        memory=16,      # 8 GiB RAM
         disk=16        # 16 GiB disk
     )
+)
 
 # Run the code securely inside the Sandbox
 response = sandbox.process.code_run('print("Hello World from code!")')
