4088d50b5f7d86bc75820aa947d1c51a9361f356
skyyyyrx <guanavera@outlook.com>
2025-07-23 11:03:39 +0700

    Create daytoni.py

---
+++ daytoni.py
@@ -0,0 +1,18 @@
+from daytona import Daytona, DaytonaConfig
+  
+# Define the configuration
+config = DaytonaConfig(api_key="dtn_65c10bd3da5ba392171806def2b21f0ad1013852708ff74c3566f34f9764bd41")
+
+# Initialize the Daytona client
+daytona = Daytona(config)
+
+# Create the Sandbox instance
+sandbox = daytona.create()
+
+# Run the code securely inside the Sandbox
+response = sandbox.process.code_run('print("Hello World from code!")')
+if response.exit_code != 0:
+  print(f"Error: {response.exit_code} {response.result}")
+else:
+    print(response.result)
+  
